bceb79e639cf3c35de04964fddd8f58c9a8aff1e
Apoorvs23 <Apoorv.Shamma@gmail.com>
2024-11-14 20:48:11 -0500

    implemented register and login apis

---
+++ src/main/java/com/apoorv/resqliciousbackend/dto/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.apoorv.resqliciousbackend.dto;
+
+import com.apoorv.resqliciousbackend.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+}
